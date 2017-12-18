f4a5d706c96e7cd861e9d24b1c3a04ad1f44ee18
dougglez <maxfield.douglas@gmail.com>
2017-12-18 11:41:16 -0700

    removing node_modules

--- config/default.config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  connectionString: "postgres://sdhcqykesokgwh:a2426ca9ef870d8d8e39d65c06440c284482f8bcb91e32cbc0d94e7601fc10eb@ec2-23-21-96-159.compute-1.amazonaws.com:5432/d2fec96h4ebado?ssl=true",
-  PORT: 8081
-};
