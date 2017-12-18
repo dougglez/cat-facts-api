842a51efa0172b863fb7794200d5db5115e8c841
dougglez <maxfield.douglas@gmail.com>
2017-12-18 16:41:17 -0700

    updates

---
+++ controllers/cat.ctrl.js
@@ -0,0 +1,40 @@
+const axios = require('axios');
+
+module.exports = {
+  get,
+  send
+};
+
+function get (req, res, next) {
+  return axios.get('https://catfact.ninja/fact')
+    .then(response => {
+      return response.data.fact;
+    })
+    .catch(error => {
+      console.log(error);
+      res.sendStatus(418);
+      return error;
+    })
+}
+
+async function send(req, res, next) {
+  try {
+    const message = await get(req, res, next);
+    slackMsg(message, res)
+  } catch (err) {
+    console.log('error:', err);
+  }
+}
+
+function slackMsg(message, res) {
+  console.log('message:', message);
+  axios.post('http://localhost:8083/api/sendmessage', {message})
+    .then(response => {
+      res.sendStatus(200);
+    })
+    .catch(error => {
+      console.log(error);
+      res.sendStatus(418);
+
+    })
+}
--- server.js
+++ server.js
@@ -3,19 +3,12 @@ const axios = require('axios');
 const app = express();
 const { PORT } = require('./config/default.config');
 const decorate = require('./middleware/decorator.middleware');
-
+const { get, send } = require('./controllers/cat.ctrl');
 decorate(app);
 
-app.get('/api/newfact', (req, res, next) => {
-  return axios.get('https://catfact.ninja/fact')
-    .then(response => {
-      res.status(200).send(response.data.fact);
-    })
-    .catch(error => {
-      console.log(error);
-      res.sendStatus(418);
-    })
-});
+app.get('/api/newfact', get);
+
+app.post('/api/sendcatslack', send);
 
 app.listen(PORT, () => {
   console.log('Ready for some dope cat facts');
