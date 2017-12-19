f5368a80180db6987e6ab9c5c77f67590f18411c
dougglez <maxfield.douglas@gmail.com>
2017-12-18 17:17:39 -0700

    new updates

--- controllers/cat.ctrl.js
+++ controllers/cat.ctrl.js
@@ -20,7 +20,7 @@ function get (req, res, next) {
 async function send(req, res, next) {
   console.log('req.body', req.body);
   console.log('req.query', req.query);
-  console.log('\n\nreq... it gonna be big\n\n', req.route.stack);
+  console.log('\n\nreq... it gonna be big\n\n', req);
   try {
     const message = await get(req, res, next);
     slackMsg(message, res)
