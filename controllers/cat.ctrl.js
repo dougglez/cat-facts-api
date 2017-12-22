846d7fe7f53cd33bded808202095343c4e7127cf
dougglez <maxfield.douglas@gmail.com>
2017-12-22 11:36:25 -0700

    set up app to work with slack-messenger as npm module

--- controllers/cat.ctrl.js
+++ controllers/cat.ctrl.js
@@ -23,14 +23,10 @@ function get (req, res, next) {
 }
 
 async function send(req, res, next) {
-  console.log('req.body', req.body);
-  console.log('req.query', req.query);
-  console.log('\n\nreq... it gonna be big\n\n', req);
-  console.log('\n\ntoken?\n\n', req.token);
   try {
     const message = await get(req, res, next);
     slackMsg.sendMessage(message);
-    res.sendStatus(200)
+    res.status(200).send(null);
   } catch (err) {
     console.log('error:', err);
     res.status(error.statusCode).send(error.message);
